8c9495fccb819eec0d8897c68e01426e860a8fba
Erica-Ku <yeskuminji@naver.com>
2023-01-18 17:56:00 +0900

    js_solution1

--- scripts/index.js
+++ scripts/index.js
@@ -56,6 +56,38 @@ const domRead = () => {
     }
 }
 
+// click 함수
+const clickBtn = (div0, bt0) => {
+    bt0.addEventListener("click", () => {
+        div0.innerHTML = `<h1>${bt0.innerHTML}</h1>`;
+        console.log(bt0.innerHTML) ;
+    });
+}
+
+//DOM 요소 수정
+const domUpdate = () => {
+    const div1 = document.querySelector("#div1") ;
+    //div1.innerHTML = "요소를 수정하고 있습니다..." ;
+    /*
+    const bt1 = document.querySelector("#bt1") ;
+    const bt2 = document.querySelector("#bt2") ;
+    const bt3 = document.querySelector("#bt3") ;
+
+    clickBtn(div1, bt1) ;
+    clickBtn(div1, bt2) ;
+    clickBtn(div1, bt3) ;
+    */
+    const btc = document.querySelectorAll(".btc") ; 
+    console.log(btc)
+    for (let bt of btc) {
+        //console.log(bt) ;      
+        bt.addEventListener("click", () => {
+            div1.innerHTML = `<h1>${bt.innerHTML}</h1>`;
+            //console.log(bt.innerHTML) ;
+        })
+    }
+}
+
 //자바스크립트 랜더링 제어
 document.addEventListener("DOMContentLoaded", () => {
     //DOM 요소 생성
@@ -63,4 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //DOM 요소 접근
     domRead();
+
+    //DOM 요소 변경
+    domUpdate();
 }) ;
