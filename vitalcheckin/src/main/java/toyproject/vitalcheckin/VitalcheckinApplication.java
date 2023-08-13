71d557f99de6395cf5d1757ff385aad84f04a9f4
Erica-Ku <yeskuminji@naver.com>
2023-08-14 01:21:49 +0900

    프로젝트 설정

---
+++ vitalcheckin/src/main/java/toyproject/vitalcheckin/VitalcheckinApplication.java
@@ -0,0 +1,13 @@
+package toyproject.vitalcheckin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalcheckinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalcheckinApplication.class, args);
+	}
+
+}
---
+++ vitalcheckin/src/test/java/toyproject/vitalcheckin/VitalcheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.vitalcheckin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalcheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
